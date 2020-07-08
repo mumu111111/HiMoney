e416a49d4d0848f9f9061cac53f77b57b0bf61ac
mumu <2646709035@qq.com>
2020-07-08 23:23:59 +0800

    '加入icon'

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+import x from 'icons/chart.svg';
+import y from 'icons/money.svg';
+import z from 'icons/tag.svg';
+console.log(x)
+console.log(y)
+
+console.log(z)
+
 const NavWrapper = styled.nav`
 // line-height: 24px;
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
 > ul {
+    // line-height: 24px; ?? 
   display: flex;
   > li {
     width: 33.3333%;
     text-align: center;
-    padding: 16px;
+    padding: 6px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .icon {
+        width: 24px;
+        height: 24px;
+    }
   }
 }
 `
@@ -18,12 +36,21 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon" fill="red">
+                        <use xlinkHref="#tag" />
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money" />
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#chart" />
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
