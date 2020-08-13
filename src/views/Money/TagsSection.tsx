e3f16deb92d2342b5576351a8a865246d1ac8e88
mumu <2646709035@qq.com>
2020-08-13 16:12:12 +0800

    'update'

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,8 @@
-
+import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
 background: #FFFFFF; padding: 12px 16px;
 flex-grow: 1; display:flex; flex-direction: column;
 justify-content: flex-end; align-items: flex-start;
@@ -17,5 +18,33 @@ justify-content: flex-end; align-items: flex-start;
   border-bottom: 1px solid #333; color: #666;
   margin-top: 8px;
 }
-`
+`;
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const onAddTag = () => {
+    const tagName = window.prompt('新增标签名为')
+    if (tagName !== null) {
+      setTags([...tags, tagName])
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    console.log(tag)
+
+  };
+  return (
+    <Wrapper>
+      <ol>
+        {
+          tags.map(tag =>
+            <li key={tag} onClick={() => onToggleTag(tag)}>{tag}</li>
+          )
+        }
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+
 export { TagsSection };
